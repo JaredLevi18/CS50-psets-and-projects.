54c8fe6627203946dc889eeb33f83820b466e0ce
Jared Levi De La Fuente Rodriguez <jk20042019@gmail.com>
2023-08-09 17:16:14 -0600

    Update population.c

--- week 1/lab/population.c
+++ week 1/lab/population.c
@@ -3,11 +3,10 @@
 
 int main(void)
 {
-    int initial;
-    int end;
+    int initial, end;
 
     do
-   {
+    {
         // TODO: Prompt for start size
         printf("Enter the initial size: ");
         scanf("%d",&initial);
@@ -16,9 +15,21 @@ int main(void)
         printf("Enter the end size: ");
         scanf("%d",&end);
     }
-    while (initial < 9 && end < 9);
+    while (initial < 9 || end < 9);
 
     // TODO: Calculate number of years until we reach threshold
+    int years = 0;
+    int born = initial / 3;
+    int die = initial / 4;
+    int population;
+
+    do
+    {
+        population = (initial + born) - die;
+        years++;
+    }
+    while (population < end);
 
     // TODO: Print number of years
+    printf("Years: %i\n", years);
 }
